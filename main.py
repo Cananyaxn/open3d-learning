59da24558fde35f8c5c4f220bf4ffc1950890693
canata <cananyaxn11@gmail.com>
2026-06-25 22:26:38 +0900

    refactor: .envからデータ読み込み

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+import os
+
 from src.loader import load_las
 from src.preprocess import downsample, estimate_normals
 from src.mesh import create_mesh
 from src.visualize import show
 
-pcd = load_las("../../research/src/las/after/experiment1.las")
+load_dotenv()
+LAS_FILE = os.getenv("LAS_FILE")
+MESH_OUTPUT = os.getenv("MESH_OUTPUT")
+
+pcd = load_las(LAS_FILE)
 pcd = downsample(pcd, voxel_size=0.05)
 pcd = estimate_normals(pcd)
 mesh = create_mesh(pcd)
